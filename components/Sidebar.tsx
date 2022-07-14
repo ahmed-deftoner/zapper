3163f44faeac665adeb99f7ea1111dc7eb0d05bf
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-07-14 12:26:17 +0500

    sidebar toggle button

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from 'react';
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
+import { ImCancelCircle } from 'react-icons/im';
 
 const Sidebar = () => {
+     const [showSidebar, setShowSidebar] = useState<Boolean>(true);
+
     return (
         <div>
-            Sidebar
+              <div
+                    className='block xl:hidden m-2 ml-4 mt-3 text-xl'
+                    onClick={() => setShowSidebar(!showSidebar)}
+                >
+                    {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
+                </div>
         </div>
     )
 }
