3a7f2609316e1e60cdfcd71df10573ce6ca7219c
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-07-14 13:11:00 +0500

    discover section

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React from 'react';
+import { NextPage } from 'next';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-const Discover = () => {
-    return (
-        <div>
-            Discover
-        </div>
-    )
-}
+import { topics } from '../utils/constants';
+
+const Discover: NextPage = () => {
+  const router = useRouter();
+  const { topic } = router.query;
+
+  const activeTopicStyle = 'md:border-2 hover:bg-primary md:border-[#F51997] px-3 py-2 rounded md:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#F51997]';
+  const topicStyle = 'md:border-2 hover:bg-primary md:border-gray-300 px-3 py-2 rounded md:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black';
+
+  return (
+    <div className='xl:border-b-2 xl:border-gray-200 pb-6'>
+      <p className='text-gray-500 font-semibold m-3 mt-4 hidden md:block'>
+        Popular Topics
+      </p>
+      <div className='flex gap-3 flex-wrap'>
+        {topics?.map((item) => (
+          <Link href={`/?topic=${item.name}`} key={item.name}>
+            <div className={topic === item.name ? activeTopicStyle : topicStyle}>
+         
+              <span className={`font-medium text-md hidden md:block capitalize`}>
+                {item.name}
+              </span>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Discover;
--- utils/constants.tsx
+++ utils/constants.tsx
@@ -1,42 +1,43 @@
 import { BsCode, BsEmojiSunglasses } from 'react-icons/bs';
 import { GiCakeSlice, GiGalaxy, GiLipstick } from 'react-icons/gi';
 import { FaPaw, FaMedal, FaGamepad } from 'react-icons/fa';
+import { GiGuitar } from 'react-icons/gi';
 
 export const topics = [
   {
-    name: 'development',
-    icon: <BsCode />,
+    name: 'Metal',
+    icon: <GiGuitar />,
   },
   {
-    name: 'comedy',
+    name: 'Pop',
     icon: <BsEmojiSunglasses />,
   },
   {
-    name: 'gaming',
+    name: 'Country',
     icon: <FaGamepad />,
   },
   {
-    name: 'food',
+    name: 'Punk',
     icon: <GiCakeSlice />,
   },
   {
-    name: 'dance',
+    name: 'Dance',
     icon: <GiGalaxy />,
   },
   {
-    name: 'beauty',
+    name: 'Hip Hop',
     icon: <GiLipstick />,
   },
   {
-    name: 'animals',
+    name: 'Rock',
     icon: <FaPaw />,
   },
   {
-    name: 'sports',
+    name: 'K-pop',
     icon: <FaMedal />,
   },
 ];
 
 export const footerList1 = ['About', 'Newsroom', 'Store', 'Contact', 'Carrers', 'ByteDance', 'Creator Directory']
-export const footerList2 = [ 'TikTik for Good','Advertise','Developers','Transparency','TikTik Rewards' ]
+export const footerList2 = [ 'Zapper for music-lovers','Advertise','Developers','Transparency','Zapper Rewards' ]
 export const footerList3 = [ 'Help', 'Safety', 'Terms', 'Privacy', 'Creator Portal', 'Community Guidelines' ]
