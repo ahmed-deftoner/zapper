e7bc6bb0924082e927d9183798521a4af8baddb3
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-07-23 12:20:25 +0500

    useauthstore

--- store/authStore.ts
+++ store/authStore.ts
@@ -6,3 +6,11 @@ const authStore = (set:any) => ({
     userProfile:null,
     addUser: (user:any) => set({ userProfile:user})
 })
+
+const useAuthStore = create((
+    persist(authStore, {
+      name: 'auth',
+    })
+  ));
+  
+  export default useAuthStore;
